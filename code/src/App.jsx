c016f31926f3738400f799b83aaf00bde991f52f
Albert <albertdang8@gmail.com>
2023-07-10 18:33:43 -0700

    start blogApp

---
+++ code/src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import HomePage from "./Pages/HomePage/HomePage";
+import CategoryArticle from "./Pages/CategoryArticle/CategoryArticle";
+
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/category/:categoryName" element={<CategoryArticle />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
