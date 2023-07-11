133da1f1b9f73426776bbe6be5927235a96ef518
Albert <albertdang8@gmail.com>
2023-07-10 23:34:24 -0700

    fix?

--- code/src/Components/Header/Header.jsx
+++ code/src/Components/Header/Header.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+
+import "./Header.css";
 
 function Header() {
+  const categories = ["Health", "Food", "Travel", "Technology"];
+
+  const navigate = useNavigate();
+
   return (
-    <div>Header</div>
-  )
+    <div className="header-container">
+      <FaHome onClick={() => navigate("/")} className="home-icon" />
+      <div className="categories-container">
+        {categories.map((item) => (
+          <Link to={`/category/${item}`} className="nav-link">
+            {item}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
