48236d4476c82b4eb16d4d9a55f7a26b4c1367d8
Albert <albertdang8@gmail.com>
2023-07-19 04:43:58 -0700

    banner work

--- code/src/Components/Banner/Banner.jsx
+++ code/src/Components/Banner/Banner.jsx
@@ -1,10 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../config/firebaseConfig";
 
 import "./Banner.css";
 
 const Banner = () => {
+
+  const [mainArticle, setMainArticle] = useState({});
+  const [otherArticles, setOtherArticles] = useState([]);
+
   useEffect(() => {
     const articlesRef = collection(db, "Articles");
 
@@ -13,11 +17,43 @@ const Banner = () => {
         const articles = res.docs.map((item) => {
           return { id: item.id, ...item.data() };
         });
+        setMainArticle(articles[0]);
+        setOtherArticles(articles.splice(1));
       })
       .catch((err) => console.log("Error: " + err));
   }, []);
 
-  return <div>Banner</div>;
+  return (
+    <div className="banner-container">
+      <div
+        className="main-article-container"
+        key={mainArticle?.id}
+        // onClick={()=>navigate(`/article/${mainArticle?.id}`)}
+        style={{ backgroundImage: `url(${mainArticle?.imageUrl})` }}
+      >
+        <div className="banner-info">
+          <h2>{mainArticle?.title}</h2>
+          <small>{mainArticle?.createdAt?.toDate().toDateString()}</small>
+        </div>
+      </div>
+      <div className="other-articles-container">
+        {otherArticles?.map((item) => (
+          <div
+            className="other-article-item"
+            key={item?.id}
+            // onClick={()=>navigate(`/article/${item?.id}`)}
+            style={{ backgroundImage: `url(${item?.imageUrl})` }}
+          >
+            <div className="banner-info">
+              <h3>{item?.title}</h3>
+              <small>{item?.createdAt?.toDate().toDateString()}</small>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
 };
 
 export default Banner;
